cb5aaeb87ccbe8ac5ac506e6da98d42e9bc960f4
eymontesinos <135139454+eymontesinos@users.noreply.github.com>
2023-08-11 10:07:46 -0500

    add view init

--- iosAppKmm/iosAppKmm/ui/splash/SplashScreen.swift
+++ iosAppKmm/iosAppKmm/ui/splash/SplashScreen.swift
@@ -1,14 +1,22 @@
+//
+//  SplashScreen.swift
+//  iosAppKmm
+//
+//  Created by MacBook Pro on 11/08/23.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
 import SwiftUI
 
 struct SplashScreen: View {
-
+    
     var body: some View {
         NavigationView {
             VStack(spacing: 16) {
                 Text("FOOD MERIA").font(.system(size: 25,weight: .bold)).foregroundColor(.white)
                 Text("BIENVENIDOS").font(.system(size: 20,weight: .bold)) .foregroundColor(.white)
                 Text("HAS TU PEDIDO AHORA").font(.system(size: 20,weight: .bold)) .foregroundColor(.white)
-
+                
             }.edgesIgnoringSafeArea(.all).frame(maxWidth: .infinity,maxHeight: .infinity)
                 .background(
                     Image(uiImage: UIImage(named: "background_splash")!)
@@ -16,7 +24,7 @@ struct SplashScreen: View {
                         .edgesIgnoringSafeArea(.all)
                 )
         }
-
+        
     }
 }
 
