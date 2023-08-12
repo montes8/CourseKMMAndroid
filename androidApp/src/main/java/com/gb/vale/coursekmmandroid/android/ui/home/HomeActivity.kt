6c061f578fd400280a6260701e187a625e5ce8e2
eymontesinos <135139454+eymontesinos@users.noreply.github.com>
2023-08-11 19:14:10 -0500

    update color

--- androidApp/src/main/java/com/gb/vale/coursekmmandroid/android/ui/home/HomeActivity.kt
+++ androidApp/src/main/java/com/gb/vale/coursekmmandroid/android/ui/home/HomeActivity.kt
@@ -5,13 +5,14 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.gb.vale.coursekmmandroid.android.MyApplicationTheme
 import com.gb.vale.coursekmmandroid.android.ui.init.splash.SplashViewModel
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class HomeActivity : ComponentActivity() {
 
     companion object {
@@ -23,7 +24,7 @@ class HomeActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                Box {
+                Box(modifier = Modifier.background(Color.White)) {
                     Text(text = "Holas home")
                 }
             }
