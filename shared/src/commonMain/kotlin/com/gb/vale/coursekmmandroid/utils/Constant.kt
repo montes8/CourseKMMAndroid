c574adbc6ef8de4a83c68867a5ae0fdab86b5a06
eymontesinos <135139454+eymontesinos@users.noreply.github.com>
2023-08-25 08:12:06 -0500

    update url

--- shared/src/commonMain/kotlin/com/gb/vale/coursekmmandroid/utils/Constant.kt
+++ shared/src/commonMain/kotlin/com/gb/vale/coursekmmandroid/utils/Constant.kt
@@ -3,7 +3,7 @@ package com.gb.vale.coursekmmandroid.utils
 
 const val EMPTY = ""
 const val PREFERENCE_TOKEN = "PREFERENCE_TOKEN"
-const val URL_BASE = "https://8236-2800-200-e3c0-1123-ec7d-fca6-2a9c-85d1.ngrok-free.app/"
+const val URL_BASE = "https://d9a5-2800-200-e3c0-1123-484c-49cf-9d59-6617.ngrok-free.app/"
 const val MY_TIME_ON = 60L
 const val MY_DELAY_SPLASH = 2000L
 
